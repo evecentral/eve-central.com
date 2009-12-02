b419761ac2ece05ed7f44135c140ca686cf0f2f5
Yann Ramin <atrus@stackworks.net>
2009-12-01 21:52:29 -0800

    Import script for new types (ported from old codebase)

---
+++ import/import_types.py
@@ -0,0 +1,65 @@
+import psycopg
+import csv
+
+db = psycopg.connect(database = 'evec', user = 'evec')
+cur = db.cursor()
+
+stat = open("types.txt")
+
+lines = stat.readlines()
+
+#del lines[0]
+#class sqldump(csv.excel):
+#    delimiter = ','
+#    quotechar = '"'
+#    escapechar = None
+#    doublequote = True
+#    skipinitialspace = False
+#    quoting = csv.QUOTE_MINIMAL
+
+
+#typeID,groupID,typeName,description,graphicID,radius,mass,volume,capacity,portionSize,raceID,basePrice,published,marketGroupID,chanceOfDuplicating
+#0    1   2     3        4     5   6     7      8        9       10    11     12        13        14
+
+csvread = csv.reader(lines)
+for fields in csvread:
+
+
+    id = long(fields[0])
+
+    name = fields[2].strip()
+
+    published = 0
+
+    description = fields[3].strip()
+    volume = float(fields[7])
+
+    try:
+        published = int(fields[12])
+    except:
+        pass
+    marketgroup = fields[13]
+    # Skip non-market items
+    if marketgroup == "null" or marketgroup == "":
+        continue
+    marketgroup = int(marketgroup)
+
+    group = int(fields[1])
+    print name,volume,published,group,marketgroup,id
+
+    cur.execute("UPDATE types SET typename = %s, published = %s, groupid = %s, marketgroup = %s WHERE typeid = %s", [name, published, group, marketgroup, id])
+    db.commit()
+
+
+    try:
+        cur.execute("INSERT INTO types (typeid, typename, size, published, groupid, marketgroup) VALUES (%s,%s,%s, %s, %s, %s)", [id, name, volume, published, group, marketgroup])
+        print "Added new type ",id
+        db.commit()
+    except:
+        db.rollback()
+
+
+
+db.commit()
+cur.execute("UPDATE types SET size = 1.1 WHERE size is NULL")
+db.commit()
