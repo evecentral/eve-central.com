9577d40ad7977fb48c5b9978f284d34003c5c4da
Yann Ramin <atrus@stackworks.net>
2012-01-09 10:47:07 -0800

    Add billions to the formatter - allow 0.99M, not 9999K

--- core/src/main/scala/com/evecentral/frontend/Formatter.scala
+++ core/src/main/scala/com/evecentral/frontend/Formatter.scala
@@ -2,16 +2,25 @@ package com.evecentral.frontend
 
 private object Thousands {
   def unapply(z: Double) : Option[String] = {
-    if (z / 1000.0 >= 1 && z / 1000.0 < 1000)
-      Some("%.02fK" format (z / 1000))
+    if (z / 1000.0 >= 1 && z / 1000.0 < 900)
+      Some("%.02fK" format (z / 1000.0))
     else None
   }
 }
 
 private object Millions {
   def unapply(z: Double) : Option[String] = {
-    if (z / 1000000.0 >= 1)
-      Some("%.02fM" format (z / 1000000))
+    if (z / 900000.0 >= 1 && z / 900000000.0 < 1)
+      Some("%.02fM" format (z / 1000000.0))
+    else
+      None
+  }
+}
+
+private object Billions {
+  def unapply(z: Double) : Option[String] = {
+    if (z / 900000000.0 >= 1)
+      Some("%.02fB" format (z / 1000000000.0))
     else
       None
   }
@@ -22,6 +31,7 @@ object Formatter {
     price match {
       case Thousands(n) => n
       case Millions(m) => m
+      case Billions(b) => b
       case y => "%.02f" format (y)
     }
   }
--- core/src/test/scala/com/evecentral/frontend/TestFormatter.scala
+++ core/src/test/scala/com/evecentral/frontend/TestFormatter.scala
@@ -12,9 +12,14 @@ class TestFormatter extends FunSuite with ShouldMatchers {
   
   test("Millions") {
     Formatter.price(1000000) should equal ("1.00M")
+    Formatter.price(990999) should equal ("0.99M")
   }
   
   test("Thousands") {
     Formatter.price(10000) should equal ("10.00K")
+  }
+
+  test("Billions") {
+    Formatter.price(10000000000.0) should equal ("10.00B")
   }
 }
