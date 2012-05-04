8e53068228116dfa5ca700ef47d88eed4f2d2d5c
Yann Ramin <atrus@stackworks.net>
2012-05-04 11:31:05 -0700

    Distance and routes

--- core/src/main/scala/com/evecentral/api/APIv3Service.scala
+++ core/src/main/scala/com/evecentral/api/APIv3Service.scala
@@ -1,36 +1,71 @@
 package com.evecentral.api
 
-import cc.spray.json._
 import cc.spray.http.MediaTypes._
 import akka.actor.{PoisonPill, Actor, Scheduler}
 import cc.spray.Directives
 import cc.spray.directives.IntNumber
 
 import com.evecentral.dataaccess._
-import com.evecentral.routes.{DistanceBetween, RouteFinderActor}
-import cc.spray.typeconversion.SprayJsonSupport
 import cc.spray.encoding.{NoEncoding, Gzip}
 
+import net.liftweb.json._
+import com.evecentral.routes.{Jump, RouteBetween, DistanceBetween, RouteFinderActor}
+import cc.spray.typeconversion.DefaultMarshallers
 
-trait APIv3Service extends Directives with DefaultJsonProtocol with SprayJsonSupport {
+
+trait APIv3Service extends Directives with DefaultMarshallers {
 
   def pathActor = { val r = (Actor.registry.actorsFor[RouteFinderActor]); r(0) }
   def ordersActor = { val r = (Actor.registry.actorsFor[GetOrdersActor]); r(0) }
 
+  def lookupSystem(text: String) : Long = {
+    try {
+      text.toLong
+    } catch {
+      case _ => StaticProvider.systemsByName(text).systemid
+    }
+  }
+
   val api3Service = {
     pathPrefix("api") {
-      path("distance/from" / IntNumber / "to" / IntNumber) {
-        (fromid, toid) =>
+      path("distance/from" / "[^/]+".r / "to" / "[^/]+".r) {
+        (fromr, tor) =>
           get {
             respondWithMediaType(`application/json`) {
               ctx =>
-              val from = StaticProvider.systemsMap(fromid)
-              val to = StaticProvider.systemsMap(toid)
-              ctx.complete((pathActor ? DistanceBetween(from, to)).as[Int] match {
-                  case Some(x) => Map("distance" -> x)
+
+                val fromid  = lookupSystem(fromr)
+                val toid = lookupSystem(tor)
+
+                import net.liftweb.json.JsonDSL._
+                val from = StaticProvider.systemsMap(fromid)
+                val to = StaticProvider.systemsMap(toid)
+                ctx.complete((pathActor ? DistanceBetween(from, to)).as[Int] match {
+                  case Some(x) => compact(render(("distance" -> x)))
                   case _ => throw new Exception("No value returned")
                 }
-              )
+                )
+            }
+          }
+      } ~
+      path("route/from" / "[^/]+".r / "to" / "[^/]+".r) {
+        (fromr, tor) =>
+          get {
+            respondWithMediaType(`application/json`) {
+              ctx =>
+                import net.liftweb.json.JsonDSL._
+
+                val fromid  = lookupSystem(fromr)
+                val toid = lookupSystem(tor)
+
+                val from = StaticProvider.systemsMap(fromid)
+                val to = StaticProvider.systemsMap(toid)
+                ctx.complete((pathActor ? RouteBetween(from, to)).as[Seq[Jump]] match {
+                  case Some(x) => compact(render(x.map {jump =>
+                    ("fromid" -> jump.from.systemid) ~ ("toid" -> jump.to.systemid) ~ (
+                      "from" -> jump.from.name) ~ ("to" -> jump.to.name) ~ ("secchange" -> jump.secChange) }
+                  ))
+                })
             }
           }
       } ~
