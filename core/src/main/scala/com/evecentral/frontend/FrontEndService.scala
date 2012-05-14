a882671a8b2f9523615de9f6fcf831df62524370
Yann Ramin <atrus@stackworks.net>
2012-05-13 22:15:21 -0700

    Orders viewer, front end mapping

--- core/src/main/scala/com/evecentral/frontend/FrontEndService.scala
+++ core/src/main/scala/com/evecentral/frontend/FrontEndService.scala
@@ -5,20 +5,28 @@ import java.util.concurrent.TimeUnit
 import akka.actor.{PoisonPill, Actor, Scheduler}
 import cc.spray.Directives
 import cc.spray.ScalateSupport
+import cc.spray.directives.Remaining
 
 trait FrontEndService extends Directives with ScalateSupport {
 
   val frontEndService = {
-      path("") {
-        get {
-          render("com/evecentral/templates/index.ssp", Map())
-        }
-      } ~
-	  path("plot") {
+	  path("") {
 		  get {
-			  render("com/evecentral/templates/plottest.ssp", Map())
+			  render("com/evecentral/templates/index.ssp", Map())
 		  }
-	  }
+	  } ~
+		  path("plot") {
+			  get {
+				  render("com/evecentral/templates/plottest.ssp", Map())
+			  }
+		  } ~
+		  path("orders" / Remaining) {
+			  remain =>
+				  get {
+					  render("com/evecentral/templates/orders.ssp", Map())
+				  }
+		  }
+
   }
 
 }
