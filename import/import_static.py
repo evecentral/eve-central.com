659fef7baedcbfba4b2127093b0cd71b0e346410
Yann Ramin <atrus@stackworks.net>
2012-08-21 16:45:51 +0000

    Import stations, after years of not doing that...

--- import/import_static.py
+++ import/import_static.py
@@ -1,6 +1,7 @@
 import psycopg2
 import sqlite3
-
+import urllib
+import xml.etree.ElementTree as ET
 import sys
 
 def import_jumps(dbs, db):
@@ -52,31 +53,55 @@ def import_systems(dbs,db):
             print "INSERT"
         db.commit()
 
+def import_station(db, stationid, stationname, solarsystem):
+    cur = db.cursor()
+    cur.execute('SELECT stationname FROM stations WHERE stationid = %s', (stationid,))
+    r = cur.fetchone()
+    if r:
+        cur.execute('UPDATE stations SET stationname = %s, systemid = %s WHERE stationid = %s', (stationname, solarsystem, stationid))
+    else:
+        cur.execute('INSERT INTO stations (stationid, stationname, systemid) VALUES (%s, %s, %s)', (stationid, stationname, solarsystem))
+        print "INSERT"
+    db.commit()
+
 def import_stations(dbs,db):
     cur = db.cursor()
     curs = dbs.cursor()
     curs.execute('SELECT stationID, stationName, solarSystemID from staStations')
     for row in curs:
+        stationid = row[0]
+        stationname = row[1]
+        solarsystem = row[2]
         print row[0],'=',row[1]
-        cur.execute('SELECT stationname FROM stations WHERE stationid = %s', (row[0],))
-        r = cur.fetchone()
-        if r:
-            cur.execute('UPDATE stations SET stationname = %s, systemid = %s WHERE stationid = %s', (row[1], row[2], row[0]))
-        else:
-            cur.execute('INSERT INTO stations (stationid, stationname, systemid) VALUES (%s, %s, %s)', (row[0], row[1], row[2]))
-            print "INSERT"
-        db.commit()
+        import_station(db, stationid, stationname, solarsystem)
 
-def main(sqle):
+def import_conq_stations(db):
+    url = "https://api.eveonline.com/eve/ConquerableStationList.xml.aspx"
+    xml_string = urllib.urlopen(url).read()
+    tree = ET.fromstring(xml_string)
+    for row in tree.iter('row'):
+        stationid = row.attrib['stationID']
+        stationname = row.attrib['stationName']
+        solarsystem = row.attrib['solarSystemID']
+        print "Conq: ",stationid,stationname,solarsystem
+        import_station(db, stationid, stationname, solarsystem)
+        
+
+def main(sqle = None, live_only = False):
     db = psycopg2.connect(database = 'evec', user = 'evec', port = 9999)
-    dbs = sqlite3.connect(sqle)
-    import_jumps(dbs, db)
-    import_regions(dbs, db)
-    import_systems(dbs, db)
-    import_stations(dbs, db)
+    import_conq_stations(db)
+    if not live_only:
+        dbs = sqlite3.connect(sqle)
+        import_jumps(dbs, db)
+        import_regions(dbs, db)
+        import_systems(dbs, db)
+        import_stations(dbs, db)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    if sys.argv[1] == "--live":
+        main(live_only = True)
+    else:
+        main(sys.argv[1])
     
 
